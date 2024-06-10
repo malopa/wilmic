52e4f923700271543c0deeb1101e0b1d086aecdf
malopa <malopalusajo@gmail.com>
2024-06-10 13:03:15 +0300

    fix mutation loading

--- app/components/SubmitButton.js
+++ app/components/SubmitButton.js
@@ -4,7 +4,7 @@ export function SubmitButton(props) {
  
   return (
     <button type="submit"  onClick={props.onClick} className='flex items-center justify-center bg-blue-800 text-white w-full p-2 mt-4 rounded-md' disabled={props.isPending}>
-      {props.mutation.isPending?<i className="pi pi-spin pi-spinner me-2" style={{ fontSize: '1rem' }}></i>:props.label}
+      {props.mutation?.isPending?<i className="pi pi-spin pi-spinner me-2" style={{ fontSize: '1rem' }}></i>:props.label}
     </button>
   )
 }
